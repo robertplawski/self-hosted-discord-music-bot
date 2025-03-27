4607ed722d248ea0f59deab7f0759dbc113e65fe
Robert Plawski <dev@robertplawski.pl>
2025-03-28 00:40:36 +0100

    Skip command working now

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -3,6 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 const { getQueueForGuild, getFromQueue } = require("../../tools/queue");
 
 const { getQueueEmbed } = require("../../tools/embed");
+const { skipItemFromQueue } = require("../../tools/voice");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,7 +12,7 @@ module.exports = {
   async execute(interaction) {
     const { guild } = interaction;
     const { fulltitle, channel, requestedBy } = getFromQueue(guild.id);
-
+    skipItemFromQueue(guild.id);
     await interaction.reply(
       `Skipping ${fulltitle} - ${channel}, requested by ${requestedBy}`
     );
